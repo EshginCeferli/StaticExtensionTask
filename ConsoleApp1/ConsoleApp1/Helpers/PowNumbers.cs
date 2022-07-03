95f16ae78c4811b0c681658a079d93dac469feb7
EshginCeferli <eshginij@code.edu.az>
2022-07-03 18:14:46 +0400

    Static Extension Task

---
+++ ConsoleApp1/ConsoleApp1/Helpers/PowNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Helpers
+{
+    public static class PowNumbers
+    {
+        public static double PowNums(this int num1, int num2) //Math metodu ile
+        {
+            return Math.Pow(num1, num2);
+        }
+        public static void PowNums2(this int num1, double num2) //Alqoritma ile
+        {
+            double result = 1;
+            for (int i = 1; i <= num2; i++)
+            {
+                result *= num1;
+            }
+            Console.WriteLine(result);
+        }
+        
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp1.Helpers;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 3;
+            Console.WriteLine(num1.PowNums(num2));
+
+            num1.PowNums2(num2);
+
+        }
+    }
+}
