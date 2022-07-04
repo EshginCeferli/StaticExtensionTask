1e677c826b1587bdbfe7746d06cbe32ab02a4bfe
EshginCeferli <107032407+EshginCeferli@users.noreply.github.com>
2022-07-04 19:39:05 +0400

    Add files via upload

---
+++ StaticExtention/ConsoleApp1/ConsoleApp1/Models/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1.Models
+{
+    public class Car
+    {
+        public static int Speed; 
+
+        static Car()
+        {
+            Speed = 500;
+            Console.WriteLine(Speed);
+        }
+
+        public static int ShowSpeed()
+        {
+            return Speed;
+        }
+    }
+}
---
+++ StaticExtention/ConsoleApp1/ConsoleApp1/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Models
+{
+    public class Person
+    {
+        public int Age;
+
+        public Person()
+        {
+            Age = 35;
+            Console.WriteLine(Age);
+        }
+    }
+
+
+
+
+}
